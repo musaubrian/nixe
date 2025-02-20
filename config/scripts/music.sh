da19792a20e4471303adfb58d72148531a0faed9
musaubrian <musaubrian15@gmail.com>
2025-02-20 18:08:38 +0300

    renix: build(29) NixOS vers: 24.11.714433.0ff09db9d034 Kernel: 6.6.76

--- config/scripts/music.sh
+++ config/scripts/music.sh
@@ -4,5 +4,9 @@ pushd ~/Music
 
 yt-dlp -f "bestaudio[ext=m4a]/bestaudio[ext=mp3]/bestaudio" $1
 
+# change title removing ytdlp's wierd `[chars]` values
+python3 ~/scripts/titly.py m
+
+echo "DONE: run ~/scripts/reformat to convert all audio to mp3"
 popd
 
--- config/scripts/reformat.sh
+++ config/scripts/reformat.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+input_dir=""
+
 if [ $# -eq 0 ]; then
-    echo "Error: Please provide an input directory"
-    echo "Usage: $0 /path/to/input/directory"
-    exit 1
+    input_dir="$HOME/Music"
+    echo "WARN: input dir not specified using default $input_dir"
+else
+    input_dir=$1
 fi
 
-input_dir=$1
 
 # Verify input is a directory
 if [ ! -d "$input_dir" ]; then
@@ -16,12 +18,11 @@ fi
 pushd "$input_dir"
 
 for file in "$input_dir"/*; do
-    # Check if the file is a video file
     if [[ $file != *.mp3 ]]; then
         output_file="${file%.*}.mp3"
-        echo "$file $output_file"
         if ffmpeg -i "$file" "$output_file"; then
             echo "Transcoded $(basename "$file") to mp3"
+            rm -v "$file"
         else
             echo "Error converting $(basename "$file")"
         fi

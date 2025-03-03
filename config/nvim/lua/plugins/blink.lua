54e313bb764f5f04b54ec3c73a1bc48fb21e24e7
musaubrian <musaubrian15@gmail.com>
2025-03-03 11:20:26 +0300

    hr: config modified

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
   dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
   version = "*",
-  -- event = "InsertEnter",
+  event = "InsertEnter",
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
